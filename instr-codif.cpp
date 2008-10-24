7005f8617d64783b6ff1d6c752ef2173b14d67cf
ed2martin@yahoo.com.br <ed2martin@yahoo.com.br@dc9b30cb-5156-0410-875e-73e64fd6dd25>
2008-10-24 01:08:48 +0000

    Instr::Codif() junta textos definidos como "a" "b" e "a"+"b" Verifica se instruções de uma classe estão na ordem correta

--- instr-codif.cpp
+++ instr-codif.cpp
@@ -282,58 +282,6 @@ static int comparaNome(const char * string1, const char * string2)
 }
 
 //------------------------------------------------------------------------------
-// Retorna um número que corresponde à prioridade do operador
-// Retorna um valor de 2 a 0x1F, ou 0 se operador inválido
-int Instr::Prioridade(int operador)
-{
-    switch (operador)
-    {
-    case exo_virgula:    return 20;
-    case exo_neg:        return 2;
-    case exo_exclamacao: return 2;
-    case exo_mul:        return 3;
-    case exo_div:        return 3;
-    case exo_porcent:    return 3;
-    case exo_add:        return 4;
-    case exo_sub:        return 4;
-    case exo_menor:      return 5;
-    case exo_menorigual: return 5;
-    case exo_maior:      return 5;
-    case exo_maiorigual: return 5;
-    case exo_igual:      return 6;
-    case exo_diferente:  return 6;
-    case exo_e:          return 7;
-    case exo_ou:         return 8;
-    case exo_igualmul:   return 9;
-    case exo_igualdiv:   return 9;
-    case exo_igualporcent: return 9;
-    case exo_igualadd:   return 9;
-    case exo_igualsub:   return 9;
-    }
-    return 0;
-}
-
-//------------------------------------------------------------------------------
-// Verifica se instrução codificada é herda e contém a classe especificada
-bool Instr::ChecaHerda(const char * instr, const char * nomeclasse)
-{
-    if (instr[0]==0 && instr[1]==0)
-        return false;
-    if (instr[2] != cHerda)
-        return false;
-    int x = *(unsigned char*)instr[3];
-    for (instr+=4; x; x--)
-    {
-        if (comparaZ(instr, nomeclasse)==0)
-            return true;
-        while (*instr)
-            instr++;
-        instr++;
-    }
-    return false;
-}
-
-//------------------------------------------------------------------------------
 // Codifica uma instrução
 // Retorna: true = conseguiu codificar com sucesso
 //          false = erro, destino contém a mensagem de erro
@@ -801,12 +749,29 @@ bool Instr::Codif(char * destino, const char * origem, int tamanho)
             }
             arg=true;
             *destino++ = ex_txt;
-            for (origem++; *origem!='\"' && destino<dest_fim-2; origem++)
+            for (origem++; destino<dest_fim-2; origem++)
             {
                 if (*origem==0)
                 {
                     copiastr(dest_ini, "Faltou fechar aspas", tamanho);
                     return false;
+                }
+                if (*origem=='\"')
+                {
+                    origem++;
+                    while (*origem==' ')
+                        origem++;
+                    if (*origem=='\"')
+                        continue;
+                    if (*origem!='+')
+                        break;
+                    const char * p = origem+1;
+                    while (*p==' ')
+                        p++;
+                    if (*p!='\"')
+                        break;
+                    origem=p;
+                    continue;
                 }
                 if (*origem=='\\')
                 {
@@ -830,7 +795,6 @@ bool Instr::Codif(char * destino, const char * origem, int tamanho)
                 }
                 *destino++ = *origem;
             }
-            origem++;
             if (destino < dest_fim-1)
                 *destino++ = 0;
             continue;
