10493ddea2e6a782f8ca8f9a01aeed72077dd831
ed2martin@yahoo.com.br <ed2martin@yahoo.com.br@dc9b30cb-5156-0410-875e-73e64fd6dd25>
2009-12-03 02:04:05 +0000

    Variável telatxt: Entende mais nomes de teclas Processa Home, End e setas para esquerda e direita + Control

--- trunk/var-telatxt.cpp
+++ trunk/var-telatxt.cpp
@@ -265,46 +265,76 @@ const char * TConsole::LerTecla()
             LerCont++;
             return LerTexto;
         }
+        DWORD state = LerEventos[LerCont].Event.KeyEvent.dwControlKeyState;
+        bool shift = state & SHIFT_PRESSED;
+        bool ctrl = state & (LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED);
         switch (LerEventos[LerCont++].Event.KeyEvent.wVirtualKeyCode)
         {
         case VK_BACK:   return "BACK";
-        case VK_TAB:    return "TAB";
-        case VK_RETURN: return "ENTER";
+        case VK_TAB:    return (shift ? "S_TAB" : "TAB");
+        case VK_RETURN: return (shift ? "S_ENTER" : "ENTER");
         case VK_ESCAPE: return "ESC";
-        case VK_PRIOR:  return "PGUP";
-        case VK_NEXT:   return "PGDN";
-        case VK_END:    return "END";
-        case VK_HOME:   return "HOME";
-        case VK_LEFT:   return "LEFT";
-        case VK_UP:     return "UP";
-        case VK_RIGHT:  return "RIGHT";
-        case VK_DOWN:   return "DOWN";
-        case VK_INSERT: return "INS";
-        case VK_DELETE: return "DEL";
-        case VK_F1:     return "F1";
-        case VK_F2:     return "F2";
-        case VK_F3:     return "F3";
-        case VK_F4:     return "F4";
-        case VK_F5:     return "F5";
-        case VK_F6:     return "F6";
-        case VK_F7:     return "F7";
-        case VK_F8:     return "F8";
-        case VK_F9:     return "F9";
-        case VK_F10:    return "F10";
-        case VK_F11:    return "F11";
-        case VK_F12:    return "F12";
+        case VK_PRIOR:  return (ctrl ? "C_PGUP" : "PGUP");
+        case VK_NEXT:   return (ctrl ? "C_PGDN" : "PGDN");
+        case VK_END:    return (ctrl ? "C_END" : "END");
+        case VK_HOME:   return (ctrl ? "C_HOME" : "HOME");
+        case VK_LEFT:   return (ctrl ? "C_LEFT" : "LEFT");
+        case VK_UP:     return (ctrl ? "C_UP" : "UP");
+        case VK_RIGHT:  return (ctrl ? "C_RIGHT" : "RIGHT");
+        case VK_DOWN:   return (ctrl ? "C_DOWN" : "DOWN");
+        case VK_INSERT: return (ctrl ? "C_INS" : "INS");
+        case VK_DELETE: return (ctrl ? "C_DEL" : "DEL");
+        case VK_F1:     return (shift ? "S_F1" : "F1");
+        case VK_F2:     return (shift ? "S_F2" : "F2");
+        case VK_F3:     return (shift ? "S_F3" : "F3");
+        case VK_F4:     return (shift ? "S_F4" : "F4");
+        case VK_F5:     return (shift ? "S_F5" : "F5");
+        case VK_F6:     return (shift ? "S_F6" : "F6");
+        case VK_F7:     return (shift ? "S_F7" : "F7");
+        case VK_F8:     return (shift ? "S_F8" : "F8");
+        case VK_F9:     return (shift ? "S_F9" : "F9");
+        case VK_F10:    return (shift ? "S_F10" : "F10");
+        case VK_F11:    return (shift ? "S_F11" : "F11");
+        case VK_F12:    return (shift ? "S_F12" : "F12");
+        case 'A':       if (ctrl) return "C_A"; break;
+        case 'B':       if (ctrl) return "C_B"; break;
+        case 'C':       if (ctrl) return "C_C"; break;
+        case 'D':       if (ctrl) return "C_D"; break;
+        case 'E':       if (ctrl) return "C_E"; break;
+        case 'F':       if (ctrl) return "C_F"; break;
+        case 'G':       if (ctrl) return "C_G"; break;
+        case 'H':       if (ctrl) return "C_H"; break;
+        case 'I':       if (ctrl) return "C_I"; break;
+        case 'J':       if (ctrl) return "C_J"; break;
+        case 'K':       if (ctrl) return "C_K"; break;
+        case 'L':       if (ctrl) return "C_L"; break;
+        case 'M':       if (ctrl) return "C_M"; break;
+        case 'N':       if (ctrl) return "C_N"; break;
+        case 'O':       if (ctrl) return "C_O"; break;
+        case 'P':       if (ctrl) return "C_P"; break;
+        case 'Q':       if (ctrl) return "C_Q"; break;
+        case 'R':       if (ctrl) return "C_R"; break;
+        case 'S':       if (ctrl) return "C_S"; break;
+        case 'T':       if (ctrl) return "C_T"; break;
+        case 'U':       if (ctrl) return "C_U"; break;
+        case 'V':       if (ctrl) return "C_V"; break;
+        case 'W':       if (ctrl) return "C_W"; break;
+        case 'X':       if (ctrl) return "C_X"; break;
+        case 'Y':       if (ctrl) return "C_Y"; break;
+        case 'Z':       if (ctrl) return "C_Z"; break;
         }
     }
 
 #else
-/*
+
+#if 0
     int ch = getc(stdin);
     if (ch<0) return 0;
     if (ch==3) return "BREAK";
     if (ch<' ') sprintf(LerTexto, "[%d]", ch);
     else sprintf(LerTexto, "%c[%d]", ch, ch);
     return LerTexto;
-*/
+#endif
     while (true)
     {
     // Lê próxima tecla
@@ -320,7 +350,7 @@ const char * TConsole::LerTecla()
             LerPont=0;
             return "ESC";
         }
-        if (ch==3) return "BREAK";
+        //if (ch==3) return "BREAK";
     // Checa teclas sem o ESC
         if (LerPont==0)
         {
@@ -333,9 +363,36 @@ const char * TConsole::LerTecla()
                 return LerTexto + 1;
             }
         // Outros caracteres
-            if (ch==9)  return "TAB";
-            if (ch==13) return "ENTER";
-            if (ch==27) LerPont=1;
+            switch (ch)
+            {
+            case 1: return "C_A";
+            case 2: return "C_B";
+            case 3: return "C_C"; // Control+C termina o programa
+            case 4: return "C_D";
+            case 5: return "C_E";
+            case 6: return "C_F";
+            case 7: return "C_G";
+            case 8: return "C_H";
+            case 9: return "TAB"; // Control+I = TAB
+            case 10: return "C_J";
+            case 11: return "C_K";
+            case 12: return "C_L";
+            case 13: return "ENTER"; // Control+M = ENTER
+            case 14: return "C_N";
+            case 15: return "C_O";
+            case 16: return "C_P";
+            case 17: return "C_Q";
+            case 18: return "C_R";
+            case 19: return "C_S";
+            case 20: return "C_T";
+            case 21: return "C_U";
+            case 22: return "C_V";
+            case 23: return "C_W";
+            case 24: return "C_X";
+            case 25: return "C_Y";
+            case 26: return "C_Z"; // Control+Z coloca em segundo plano
+            case 27: LerPont=1; break; // ESC
+            }
             continue;
         }
     // Obtém string de teclas com o ESC
@@ -353,7 +410,7 @@ const char * TConsole::LerTecla()
         }
         if (LerPont < 14)
             LerTexto[LerPont++] = ch;
-        if (ch < 'A')
+        if (ch < 'A' || ch=='[')
             continue;
         LerTexto[LerPont] = 0;
         LerPont = 0;
@@ -363,28 +420,59 @@ const char * TConsole::LerTecla()
     // Códigos começam com ESC + [ ou ESC + O e terminam com caracter >= A
     // Após o ESC, obrigatoriamente em ordem alfabética:
         const char * cod_esc[] = {
-            "OP\0" "F1",
-            "OQ\0" "F2",
-            "OR\0" "F3",
-            "OS\0" "F4",
-            "[15~\0" "F5",
-            "[17~\0" "F6",
-            "[18~\0" "F7",
-            "[19~\0" "F8",
-            "[20~\0" "F9",
-            "[21~\0" "F10",
-            "[23~\0" "F11",
-            "[24~\0" "F12",
-            "[2~\0" "INS",
-            "[3~\0" "DEL",
-            "[5~\0" "PGUP",
-            "[6~\0" "PGDN",
-            "[A\0" "UP",
-            "[B\0" "DOWN",
-            "[C\0" "RIGHT",
-            "[D\0" "LEFT",
-            "[F\0" "END",
-            "[H\0" "HOME" };
+            "O2P\0"     "S_F1",
+            "O2Q\0"     "S_F2",
+            "O2R\0"     "S_F3",
+            "O2S\0"     "S_F4",
+            "OM\0"      "S_ENTER",
+            "OP\0"      "F1",
+            "OQ\0"      "F2",
+            "OR\0"      "F3",
+            "OS\0"      "F4",
+            "[15;2~\0"  "S_F5",
+            "[15~\0"    "F5",
+            "[17;2~\0"  "S_F6",
+            "[17~\0"    "F6",
+            "[18;2~\0"  "S_F7",
+            "[18~\0"    "F7",
+            "[19;2~\0"  "S_F8",
+            "[19~\0"    "F8",
+            "[1;5A\0"   "C_UP",
+            "[1;5B\0"   "C_DOWN",
+            "[1;5C\0"   "C_RIGHT",
+            "[1;5D\0"   "C_LEFT",
+            "[1;5F\0"   "C_END",
+            "[1;5H\0"   "C_HOME",
+            "[1~\0"     "HOME",
+            "[20;2~\0"  "S_F9",
+            "[20~\0"    "F9",
+            "[21;2~\0"  "S_F10",
+            "[21~\0"    "F10",
+            "[23;2~\0"  "S_F11",
+            "[23~\0"    "F11",
+            "[24;2~\0"  "S_F12",
+            "[24~\0"    "F12",
+            "[2;5~\0"   "C_INS",
+            "[2~\0"     "INS",
+            "[3;5~\0"   "C_DEL",
+            "[3~\0"     "DEL",
+            "[4~\0"     "END",
+            "[5;5~\0"   "C_PGUP",
+            "[5~\0"     "PGUP",
+            "[6;5~\0"   "C_PGDN",
+            "[6~\0"     "PGDN",
+            "[A\0"      "UP",
+            "[B\0"      "DOWN",
+            "[C\0"      "RIGHT",
+            "[D\0"      "LEFT",
+            "[F\0"      "END",
+            "[H\0"      "HOME",
+            "[Z\0"      "S_TAB",
+            "[[A\0"     "F1",
+            "[[B\0"     "F2",
+            "[[C\0"     "F3",
+            "[[D\0"     "F4",
+            "[[E\0"     "F5" };
         int ini = 0;
         int fim = sizeof(cod_esc) / sizeof(cod_esc[0]) - 1;
         while (ini <= fim)
@@ -592,7 +680,7 @@ void TConsole::CursorCol(int valor)
     ColAtual += valor;
     if (ColAtual < 0)
         ColAtual = 0;
-    if (ColAtual >= LinTotal)
+    if (ColAtual >= ColTotal)
         ColAtual = ColTotal-1;
     ini_linha = false;
 }
@@ -812,42 +900,64 @@ void TConsole::ProcTecla(const char * texto)
         EnvTxt(linha + col_linha + ColEditor - 1, 1); // Mostra caracter
         return;
     }
+// ENTER
+    if (strcmp(texto, "ENTER")==0)
+    {
+        col_linha = 0;
+        tam_linha = 0;
+        ColEditor = 0;
+        CursorIni();  // Posicina o cursor no início da linha
+        LimpaFim();   // Limpa a linha
+        return;
+    }
 // Esquerda
     if (strcmp(texto, "LEFT")==0)
     {
-        if (ColEditor>0)
-        {
-            ColEditor--;
-            CursorCol(-1);  // Recua cursor
-        }
-        else if (col_linha>0)
-        {
-            col_linha--;
-            InsereCol(1);
-            EnvTxt(linha + col_linha, 1); // Mostra caracter
-            CursorCol(-1);          // Cursor no início da linha
-        }
+        ProcTeclaCursor(ColEditor + col_linha - 1);
         return;
     }
 // Direita
     if (strcmp(texto, "RIGHT")==0)
     {
-        if (ColEditor + col_linha >= tam_linha)
-            return;
-        if (ColEditor < ColTotal-2)
-        {
-            ColEditor++;
-            CursorCol(1);  // Recua cursor
-            return;
-        }
-        col_linha++;
-        CursorIni(); // Cursor no início da linha
-        ApagaCol(1); // Apaga 1 caracter
-        CursorCol(ColEditor); // Posiciona cursor
-        if (col_linha + ColEditor >= tam_linha)
-            return;
-        EnvTxt(linha + col_linha + ColEditor, 1); // Mostra caracter
-        CursorCol(-1); // Volta cursor
+        ProcTeclaCursor(ColEditor + col_linha + 1);
+        return;
+    }
+// Home
+    if (strcmp(texto, "HOME")==0)
+    {
+        ProcTeclaCursor(0);
+        return;
+    }
+// End
+    if (strcmp(texto, "END")==0)
+    {
+        ProcTeclaCursor(tam_linha);
+        return;
+    }
+// Control + esquerda
+    if (strcmp(texto, "C_LEFT")==0)
+    {
+        int col = ColEditor + col_linha - 1;
+        for (; col>=0; col--)
+            if (linha[col] >= '0')
+                break;
+        for (; col>=0; col--)
+            if (linha[col] < '0')
+                break;
+        ProcTeclaCursor(col+1);
+        return;
+    }
+// Control + direita
+    if (strcmp(texto, "C_RIGHT")==0)
+    {
+        int col = ColEditor + col_linha + 1;
+        for (; col<(int)tam_linha; col++)
+            if (linha[col] < '0')
+                break;
+        for (; col<(int)tam_linha; col++)
+            if (linha[col] >= '0')
+                break;
+        ProcTeclaCursor(col);
         return;
     }
 // DEL
@@ -897,16 +1007,67 @@ void TConsole::ProcTecla(const char * texto)
         CursorCol(-move-1);
         return;
     }
-// ENTER
-    if (strcmp(texto, "ENTER")==0)
+}
+
+//---------------------------------------------------------------------------
+void TConsole::ProcTeclaCursor(int coluna)
+{
+    bool redesenha = false;
+// Acerta limites da coluna
+    if (coluna < 0)
+        coluna = 0;
+    if (coluna > (int)tam_linha)
+        coluna = (int)tam_linha;
+// Obtém a coluna do cursor na tela
+    coluna -= col_linha;
+// Mover para esquerda
+    if (coluna < 0)
     {
-        col_linha = 0;
-        tam_linha = 0;
-        ColEditor = 0;
-        CursorIni();  // Posicina o cursor no início da linha
-        LimpaFim();   // Limpa a linha
-        return;
+        int move = -coluna;
+        coluna = 0;
+        col_linha -= move;
+        if (move < (int)ColTotal-1) // Cabe na tela
+        {
+            CursorIni();
+            InsereCol(move);
+            EnvTxt(linha + col_linha, move);
+        }
+        else // Não cabe na tela
+            redesenha = true;
     }
+// Mover para direita
+    if (coluna > (int)ColTotal-2)
+    {
+        int move = coluna - (ColTotal-2);
+        coluna = ColTotal-2;
+        col_linha += move;
+        if (move < (int)ColTotal-1) // Cabe na tela
+        {
+            CursorIni();
+            ApagaCol(move);
+            int pos = ColTotal - 1 - move;
+            CursorCol(pos);
+            pos += col_linha;
+            if (move > (int)tam_linha - pos)
+                move = (int)tam_linha - pos;
+            EnvTxt(linha + pos, move);
+        }
+        else // Não cabe na tela
+            redesenha = true;
+    }
+// Atualizar toda a linha de edição
+    if (redesenha)
+    {
+        int total = tam_linha - col_linha;
+        if (total > (int)ColTotal-1)
+            total = (int)ColTotal-1;
+        CursorIni();
+        EnvTxt(linha + col_linha, total);
+        LimpaFim();
+    }
+// Acerta posição do cursor
+    ColEditor = coluna;
+    CursorCol(ColEditor - ColAtual);
 }
 
 //---------------------------------------------------------------------------
