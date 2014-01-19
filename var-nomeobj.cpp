c4b5798637d090b4b8a4db3c5d5a2885b887e620
ed2martin@yahoo.com.br <ed2martin@yahoo.com.br@dc9b30cb-5156-0410-875e-73e64fd6dd25>
2014-01-19 23:45:32 +0000

    No IntMUD: Função nomeobj.ini aceita um terceiro argumento, que é a palavra que se usada significa todos os objetos Versão do IntMUD mudou para 1.02c No MUD: Comandos Pegar, Soltar, Vestir e Remover aceitam a palavra tudo, que significa todos os objetos; no entanto, dependendo da natureza dos objetos, podem não ser todos Comandos Listarsav e Apagarsav, para ver e apagar arquivos de jogos salvos Comandos Salvar e Senha, para salvar o jogo e para mudar a senha Comando Gritar, para enviar mensagem para os jogadores que estiverem na mesma área Comandos Teleentra e Telesai, para administradores mudarem as mensagens de quando se teletransportam com o comando Tele Ajuda do comando Fim foi melhorada

--- var-nomeobj.cpp
+++ var-nomeobj.cpp
@@ -28,45 +28,48 @@ bool TVarNomeObj::Func(TVariavel * v, const char * nome)
             return Instr::CriarVarInt(0);
         }
     // Prepara variáveis
-        int valor = (Instr::VarAtual - v < 2 ? 1 : v[2].getInt());
-        char mens[4096];
-        const char * txt = v[1].getTxt();
-        while (*txt==' ') txt++;
-        char * p = mens;
-        for (; *txt && p<mens+sizeof(mens)-1; p++,txt++)
-            *p = tabNOMEOBJ[*(unsigned char*)(txt)];
-        *p=0;
-        // Checa fim do nome
-        if (*mens==0)
+        if (*NomeObj)
         {
-            Instr::ApagarVar(v);
-            return Instr::CriarVarInt(0);
-        }
+            char mens[4096];
+            const char * txt = v[1].getTxt();
+            while (*txt==' ') txt++;
+            char * p = mens;
+            for (; *txt && p<mens+sizeof(mens)-1; p++,txt++)
+                *p = tabNOMEOBJ[*(unsigned char*)(txt)];
+            *p=0;
+            // Checa fim do nome
+            if (*mens==0)
+            {
+                Instr::ApagarVar(v);
+                return Instr::CriarVarInt(0);
+            }
     // Verifica se é o nome procurado
     // Todas as palavras de NomeObj devem estar em mens
-        txt = NomeObj;
-        while (*txt)
-        {
-            const char * lista = mens;
-            while (true)
+            txt = NomeObj;
+            while (*txt)
             {
-                const char * str = txt;
-                while (*str && *str!=' ' && *str==*lista)
-                    str++, lista++;
-                if (*str==0 || *str==' ') // Encontrou
-                    break;
-                while (*lista && *lista!=' ') lista++;
-                while (*lista==' ') lista++;
-                if (*lista==0) // Fim da lista: não encontrou
+                const char * lista = mens;
+                while (true)
                 {
-                    Instr::ApagarVar(v);
-                    return Instr::CriarVarInt(0);
+                    const char * str = txt;
+                    while (*str && *str!=' ' && *str==*lista)
+                        str++, lista++;
+                    if (*str==0 || *str==' ') // Encontrou
+                        break;
+                    while (*lista && *lista!=' ') lista++;
+                    while (*lista==' ') lista++;
+                    if (*lista==0) // Fim da lista: não encontrou
+                    {
+                        Instr::ApagarVar(v);
+                        return Instr::CriarVarInt(0);
+                    }
                 }
+                while (*txt && *txt!=' ') txt++;
+                while (*txt==' ') txt++;
             }
-            while (*txt && *txt!=' ') txt++;
-            while (*txt==' ') txt++;
         }
     // Checa se é o item inicial
+        int valor = (Instr::VarAtual - v < 2 ? 1 : v[2].getInt());
         if (Inicio >= valor)
         {
             Inicio -= valor;
@@ -92,54 +95,89 @@ bool TVarNomeObj::Func(TVariavel * v, const char * nome)
     // Checa o número de variáveis
         if (Instr::VarAtual - v < 1)
             return false;
+    // Obtém o texto que se refere a todos os itens
+        char nometudo[200];
+        *nometudo = 0;
+        if (Instr::VarAtual >= v+3)
+        {
+            const char * o = v[3].getTxt();
+            char * d = nometudo;
+            for (; *o && d < nometudo + sizeof(nometudo) - 1; o++,d++)
+                *d = tabNOMEOBJ[*(unsigned char*)o];
+            *d = 0;
+        }
     // Obtém o texto
         Inicio = 0;
         const char * txt = v[1].getTxt();
         while (*txt==' ')
             txt++;
-        while (*txt>='0' && *txt<='9')
+        if (*nometudo)
         {
-        // Obtém o número
-            const char * p = txt;
-            int num = 0;
-            for (; *p>='0' && *p<='9'; p++)
-                if (num<1000000)
-                    num = num * 10 + *p - '0';
-        // O número é zero
-            if (num==0)
-                break;
-        // É a quantidade de itens
-            if (*p==' ' && Total==0)
-            {
-                while (*p==' ')
-                    p++;
-                if (*p==0)  // Somente o número não pode ser quantidade
-                    break;
-                txt=p;
-                Total=num;
-                continue;
-            }
-        // É o número do primeiro item
-            if (*p=='.')
-                Inicio=num-1, txt=p+1;
-        // É o nome do item
-            break;
+            const char * p1 = txt;
+            const char * p2 = nometudo;
+            while (*p1 && tabNOMEOBJ[*(unsigned char*)p1] == *p2)
+                p1++,p2++;
+            if (*p1=='.' && *p2==0)
+                Total=0x7FFFFFFF, txt=p1+1;
         }
-        // Copia o texto
-        while (*txt==' ')
-            txt++;
-        char * p = NomeObj;
-        for (; *txt && p<NomeObj+sizeof(NomeObj)-1; p++,txt++)
-            *p = tabNOMEOBJ[*(unsigned char*)(txt)];
-        *p=0;
-        // Obtém Total
+        if (Total == 0)
+            while (*txt>='0' && *txt<='9')
+            {
+            // Obtém o número
+                const char * p = txt;
+                int num = 0;
+                for (; *p>='0' && *p<='9'; p++)
+                    if (num<1000000)
+                        num = num * 10 + *p - '0';
+            // O número é zero
+                if (num==0)
+                    break;
+            // É a quantidade de itens
+                if (*p==' ' && Total==0)
+                {
+                    while (*p==' ')
+                        p++;
+                    if (*p==0)  // Somente o número não pode ser quantidade
+                        break;
+                    txt=p;
+                    Total=num;
+                    continue;
+                }
+            // É o número do primeiro item
+                if (*p=='.')
+                    Inicio=num-1, txt=p+1;
+            // É o nome do item
+                break;
+            }
+    // Copia o texto
+        {
+            while (*txt==' ')
+                txt++;
+            char * p = NomeObj;
+            for (; *txt && p<NomeObj+sizeof(NomeObj)-1; p++,txt++)
+                *p = tabNOMEOBJ[*(unsigned char*)(txt)];
+            *p=0;
+        }
+    // Checa se o texto é nulo
+        if (*NomeObj == 0)
+        {
+            Total = 0;
+            return false;
+        }
+    // Checa se digitou a palavra que corresponde a todos os itens
+        if (Instr::VarAtual >= v+3)
+            if (strcmp(NomeObj, nometudo)==0)
+            {
+                *NomeObj = 0;
+                if (Total <= 0)
+                    Total = 0x7FFFFFFF;
+            }
+    // Obtém Total
         if (Total <= 0)
-            Total=1;
+            Total = 1;
         int x = (Instr::VarAtual - v < 2 ? 1 : v[2].getInt());
-        if (Total>x)
-            Total=x;
-        if (*NomeObj==0)
-            Total=0;
+        if (Total > x)
+            Total = x;
         return false;
     }
     return false;
